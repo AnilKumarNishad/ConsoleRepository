02069a05260074128d321483924692977bac3f16
AnilKumarNishad <anilkumarcse.nishad@gmail.com>
2018-05-07 14:39:25 +0530

    ConsoleAppFactory

---
+++ ConsoleAppFactory/FolderFactory.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Permissions;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleAppFactory
+{
+    public class FolderFactory
+    {
+        public ICollection<ConsoleAppEntity.FolderDetails> GetFolderDetails()
+        {
+            
+            string DriveName = Path.GetPathRoot(Environment.SystemDirectory);
+            string TextWriterFilePath = DriveName + $"Temp";
+            List<ConsoleAppEntity.FolderDetails> folderDetailList = new List<ConsoleAppEntity.FolderDetails>();
+
+            DirectoryInfo d = new DirectoryInfo(DriveName);
+            DirectoryInfo[] dis = d.GetDirectories();
+            Console.WriteLine("Please wait while we calculating folder size...");
+            foreach (var item in dis)
+            {
+                using (ConsoleAppEntity.FolderDetails folderDetails = new ConsoleAppEntity.FolderDetails())
+                {
+                    var fileSize = DirSize(item);
+                    folderDetails.FolderName = item.FullName;
+                    folderDetails.FolderSize = fileSize;
+                    folderDetails.FolderSizeInBytes = fileSize + " bytes";
+                    folderDetailList.Add(folderDetails);
+                }
+            }
+            TextFileWriterFactory textFileWriter = new TextFileWriterFactory();
+            textFileWriter.CreateFile(TextWriterFilePath, "TempTextFile.txt");
+           var details= folderDetailList.OrderByDescending(C => C.FolderSize);
+            foreach (var item in details)
+            {
+                textFileWriter.WriteText(TextWriterFilePath+ "\\TempTextFile.txt", "File Name:" + item.FolderName +" ,File Size:"+item. FolderSizeInBytes);
+
+            }
+            return details.ToList();
+        }
+        private long DirSize(DirectoryInfo d)
+        {
+            long size = 0;
+            try
+            {
+
+            FileInfo[] fis = d.GetFiles();
+            foreach (FileInfo fi in fis)
+            {
+                size += fi.Length;
+            }
+            // Add subdirectory sizes.
+            DirectoryInfo[] dis = d.GetDirectories();
+            foreach (DirectoryInfo di in dis)
+            {
+                size += DirSize(di);
+            }
+            }
+            catch (Exception)
+            {
+
+                return size;
+            }
+           
+            return size;
+        }
+        
+        private List<String> DirSearch(string sDir)
+        {
+            List<String> files = new List<String>();
+            try
+            {
+                foreach (string f in Directory.GetFiles(sDir))
+                {
+                    files.Add(f);
+                }
+                foreach (string d in Directory.GetDirectories(sDir))
+                {
+                    files.AddRange(DirSearch(d));
+                }
+            }
+            catch (System.Exception)
+            {
+                
+            }
+
+            foreach (var item in files)
+            {
+                Console.WriteLine(item);
+            }
+            return files;
+        }
+
+
+    }
+}
---
+++ ConsoleAppFactory/TextFileWriterFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+namespace ConsoleAppFactory
+{
+    public class TextFileWriterFactory
+    {
+        public void WriteText(string strPath, string Text)
+        {  if (File.Exists(strPath))
+            {
+                using (StreamWriter sw =  File.AppendText(strPath))
+                {
+                    sw.Write(Environment.NewLine);
+                    sw.Write("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                    sw.Write(Environment.NewLine);
+                    sw.Write(Text);
+                }
+            }
+        }
+
+        public void CreateFile(string strPath,string FileName)
+        {            
+            if (!File.Exists(System.IO.Path.Combine(strPath, FileName)))
+            {
+                System.IO.Directory.CreateDirectory(strPath);
+                using (FileStream fs = new FileStream(System.IO.Path.Combine(strPath, FileName), FileMode.OpenOrCreate))
+                {
+                    using (BinaryWriter w = new BinaryWriter(fs))
+                    {
+                            w.Write("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"+ Environment.NewLine);
+                            w.Write(" File Created date " + DateTime.Now.ToShortDateString() + Environment.NewLine);
+                            w.Write(" File Created On "+ DateTime.Now.ToShortTimeString() + Environment.NewLine);
+                            w.Write(" !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                    }
+                }
+               
+            }
+        }
+
+
+    }
+}
