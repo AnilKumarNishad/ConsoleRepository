536754fc361ae6d49334d0d1ed1d3c5f4fc57f3a
AnilKumarNishad <anilkumarcse.nishad@gmail.com>
2018-05-07 14:37:09 +0530

    console app

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleAppFactory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FolderFactory folderFactory = new FolderFactory();
+            var fileList = folderFactory.GetFolderDetails();  
+
+        }
+    }
+}
