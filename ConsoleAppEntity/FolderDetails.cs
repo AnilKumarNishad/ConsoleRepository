8c2723821cf5420e29ac7aee1d95312182f2b6bb
AnilKumarNishad <anilkumarcse.nishad@gmail.com>
2018-05-07 14:38:29 +0530

    ConsoleApp Entity

---
+++ ConsoleAppEntity/FolderDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppEntity
+{
+  public  class FolderDetails :IDisposable
+    {
+        public string FolderName { get; set; }
+        public string FolderSizeInBytes { get; set; }
+        public long FolderSize { get; set; }
+
+        public void Dispose()
+        {
+            
+            GC.SuppressFinalize(this);
+        }
+    }
+}
